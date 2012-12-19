35b0a997df3b09a7e002cc8c2332aaf800013af8
Mike Larsson <mikelarssonftw@gmail.com>
2012-12-19 12:16:04 -0500

    Moving laserloaf into examples folder

---
+++ examples/laserloaf/tileboard.loafcat.js
@@ -0,0 +1,53 @@
+(function(Tileboard) {
+
+  function parseCommand(key) {
+    var cmd = undefined;
+    if(key.match(/[qweasdzxc]/i)) {
+      cmd = { x: 0, y: 0 };
+      if(key.match(/[qaz]/i)) cmd.x = -1;
+      else if(key.match(/[edc]/i)) cmd.x = 1;
+      if(key.match(/[zxc]/i)) cmd.y = -1;
+      else if(key.match(/[qwe]/i)) cmd.y = 1;
+    }
+    return cmd;
+  }
+
+  function findLocation(node) {
+    var classes = node.parentNode.classList;
+    for (var i = 0; i < classes.length; i++) {
+      if (classes[i].match(/^[a-h][1-8]$/)) return classes[i];
+    }
+    return undefined;
+  }
+
+  function getCoords(start, delta) {
+    var targ = String.fromCharCode(start[0].charCodeAt() + delta.x) 
+      + (parseInt(start[1]) + delta.y);
+    if(targ.match(/[a-h][1-8]/i)) {
+      return targ;
+    }
+    else {
+      return undefined;
+    }
+  }
+
+  function move(node, delta) {
+    var loc = findLocation(node);
+    var targLoc = getCoords(loc, delta);
+    if(targLoc) {
+      var targ = document.querySelector('.board .' + targLoc);
+      targ.appendChild(node);
+    }
+  }
+
+  document.onkeydown = function(evt){
+    if(!document.querySelector('.editing-css')){
+      var key = String.fromCharCode(evt.keyCode);
+      var delta = parseCommand(String.fromCharCode(evt.keyCode));
+      if(delta) {
+        move(document.querySelector('.board .loafcat'), delta);
+        return false;
+      }
+    }
+  };
+})(Tileboard);
